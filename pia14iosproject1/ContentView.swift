3209c455b6b0cf9fc584d732f18a35be4b75e552
Muhammad Jawad <jawadbqa@gmail.com>
2025-11-19 13:54:43 +0100

    Initial Commit

---
+++ pia14iosproject1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pia14iosproject1
+//
+//  Created by Muhammad Jawad on 2025-11-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pia14iosproject1/pia14iosproject1App.swift
@@ -0,0 +1,17 @@
+//
+//  pia14iosproject1App.swift
+//  pia14iosproject1
+//
+//  Created by Muhammad Jawad on 2025-11-19.
+//
+
+import SwiftUI
+
+@main
+struct pia14iosproject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
